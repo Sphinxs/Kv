8970cd33dd51a5d0279f0caae327ad78788f7cc2
Walter <Sphinxs@users.noreply.github.com>
2017-12-04 17:03:27 -0200

    Ambient

--- Ambient.sh
+++ Ambient.sh
@@ -4,19 +4,16 @@
 sudo apt-get install -y python3-pip build-essential git python python-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev dirmngr
 
 
-''' Install :
-
-For system like Elementary : sudo apt install software-properties-common gdebi -y
-
+# Packages
 
 Debian : sudo echo "deb http://ppa.launchpad.net/kivy-team/kivy/ubuntu trusty main" >> /etc/apt/sources.list && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A863D2D6
 
 Ubuntu : sudo add-apt-repository ppa:kivy-team/kivy
 
-'''
+
+# Install
 
 apt update & apt upgrade & apt install python3-kivy
-
 
 
 # Extras
@@ -26,13 +23,3 @@ pip3 install docutils pygments Cython
 pip3 install --upgrade pip wheel setuptools docutils pygments Cython
 
 apt install python-pygame && apt install --upgrade python-pygame
-
-pip3 install --upgrade kivy
-
-    # Softwares
-
-        # Atom
-
-            wget https://atom.io/download/deb -O atom.deb
-
-            sudo dpkg -i atom.deb & atom
